74ca13447bbaf08d0ca526781ac9e4228eb652b2
Evgeny <vasilkalin@gmail.com>
2021-03-24 15:32:24 +0500

    work variant with exception

--- src/SortString.java
+++ src/SortString.java
@@ -5,22 +5,32 @@ public class SortString {
         System.out.println("Please, enter the string ");
         Scanner text = new Scanner(System.in);
         String str = text.nextLine();
+        try {
+            if (str.equals("") || (!str.matches("(.*)1(.*)|(.*)2(.*)|(.*)3(.*)|(.*)4(.*)|(.*)5(.*)|(.*)6(.*)|(.*)7(.*)|(.*)8(.*)|(.*)9(.*)|(.*)I(.*)|(.*)V(.*)|(.*)X(.*)")) ) {
+                throw new MyException();
+            }
+        } catch (MyException e) {
+            System.out.println("Please, try again");
+            e.printStackTrace();
+        }
 //  check string: CASE when string has ARAB digits
         boolean teststr = str.matches("(.*)1(.*)|(.*)2(.*)|(.*)3(.*)|(.*)4(.*)|(.*)5(.*)|(.*)6(.*)|(.*)7(.*)|(.*)8(.*)|(.*)9(.*)");
-        if(teststr){
+        if(teststr) {
             String items[] = str.split("\\s*(\\s|\\+|-|/|\\*)\\s*");
             int ent[] = new int[items.length];
-            for(int i=0; i < items.length; i++){
-                try{
+            for (int i = 0; i < items.length; i++) {
+                try {
                     ent[i] = Integer.parseInt(items[i]);
-                }catch(NumberFormatException e){
+                } catch (NumberFormatException e) {
                     //Error
                 }
             }
             int arr1 = ent[0];
             int arr2 = ent[1];
 
+
             System.out.println(Operation.operateArab(str, arr1, arr2));
+
         }
 // Check string CASE when string is ROME digits
         else {
